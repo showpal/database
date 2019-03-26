24b34f146a23e6a0cdcc6901b6ee45ae5a0e5504
davidjeremie <jeremie_david@berkeley.edu>
2019-03-25 22:51:40 -0700

    Add trainer, classes and horse_classes table

--- create_db.sql
+++ create_db.sql
@@ -3,3 +3,25 @@ CREATE TABLE riders (rider_id INT AUTO_INCREMENT PRIMARY KEY, rider_fname VARCHA
        rider_lname VARCHAR(100), sex CHAR(1), birth DATE, nationality VARCHAR(100), trainer_id INT);
 CREATE TABLE horses (horse_id INT AUTO_INCREMENT PRIMARY KEY, horse_name VARCHAR(100) NOT NULL,
        owner_id INT, sex CHAR(1), birth DATE, trainer_id INT);
+create table trainer (
+	id_trainer varchar(10) PRIMARY KEY,
+    trainer_first_name varchar(250) NOT NULL,
+    trainer_last_name varchar(250) NOT NULL
+);
+create table classes (
+	id_classes varchar(10) PRIMARY KEY,
+    classes_name varchar(250) NOT NULL,
+    start_time varchar(250) NULL,
+    end_time varchar(250) NULL,
+    details varchar(250) NULL,
+    ring varchar(250) NOT NULL,
+    division varchar(250) NOT NULL
+    );
+
+
+create table horses_classes (
+	id_classes varchar(10) NOT NULL,
+    start_time varchar(250) NOT NULL,
+    horse_id varchar(250) NOT NULL,
+    rider_id varchar(250) NOT NULL
+);
--- test_data_populate.sql
+++ test_data_populate.sql
@@ -10,3 +10,32 @@ INSERT INTO horses (horse_name, owner_id, sex, birth, trainer_id) VALUES ('Justi
 INSERT INTO horses (horse_name, owner_id, sex, birth, trainer_id) VALUES ('Bitcoin', '3', 'G', '2013-01-01', '2');
 INSERT INTO horses (horse_name, owner_id, sex, birth, trainer_id) VALUES ('Idle Dice', '1', 'G', '1975-01-01', '1');
 INSERT INTO horses (horse_name, owner_id, sex, birth, trainer_id) VALUES ('JD Heavensent', '2', 'M', '2010-02-03', '3');
+insert into trainer (id_trainer, trainer_first_name, trainer_last_name)
+values ('01', 'Megan', 'McClarty');
+
+insert into trainer (id_trainer, trainer_first_name, trainer_last_name)
+values ('02', 'Jeremie', 'David');
+
+insert into trainer (id_trainer, trainer_first_name, trainer_last_name)
+values ('03', 'Marc', 'Capelo');
+
+
+insert into horses_classes (id_classes, start_time, horse_id, rider_id)
+values ('01', '12:40', 'Justify', 'Megg');
+
+insert into horses_classes (id_classes, start_time, horse_id, rider_id)
+values ('01', '12:42', 'Milkshake', 'Megg');
+
+insert into horses_classes (id_classes, start_time, horse_id, rider_id)
+values ('02', '12:44', 'Bitcoin', 'Megg');
+
+insert into horses_classes (id_classes, start_time, horse_id, rider_id)
+values ('02', '12:46', 'Boeuf-Bourguignon', 'Megg');
+
+
+insert into classes (id_classes, classes_name, start_time, end_time, details, ring, division)
+values ('01', 'jumping', '12:40', '13:40', 'Horse jumping', '08', 'A');
+
+
+insert into classes (id_classes, classes_name, start_time, end_time, details, ring, division)
+values ('02', 'skydiving', '14:40', '16:40', 'Horse skydiving', '09', 'D');
